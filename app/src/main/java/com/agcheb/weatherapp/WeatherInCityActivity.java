bf9bf0a401ae4ed04b015944e6ff34070817d060
agcheb <agchebykin@gmail.com>
2017-11-02 12:02:28 +0300

    добавлен новый экран

---
+++ app/src/main/java/com/agcheb/weatherapp/WeatherInCityActivity.java
@@ -0,0 +1,13 @@
+package com.agcheb.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherInCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_in_city);
+    }
+}
